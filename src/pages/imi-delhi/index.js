97c22fdc6f0ca71ed7af7d608a1207fc80d4889f
maheshwar <maheshroy1811@gmail.com>
2023-11-01 22:50:12 +0530

    new changes

--- src/pages/imi-delhi/index.js
+++ src/pages/imi-delhi/index.js
@@ -1,288 +1,43 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Pagination } from "swiper";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import BreadCrumb from "../../../components/common/BreadCrumb";
+import Footer from "../../../components/common/Footer";
+import CourseDetailsArea from "../../../components/CourseDetails/CourseDetailsArea";
+import Header from "../../../components/Home/Header";
+import SEO from "../../../components/seo";
+import { useGetCourseQuery } from "../../../redux/api/apiSlice";
+import DetailsTabItems from "../../../components/CourseDetails/DetailsTabItems";
 
-SwiperCore.use([Pagination]);
-import Link from "next/link";
+const CourseDetails = () => {
+  const {
+    data: course,
+    isLoading,
+    isError,
+  } = useGetCourseQuery("6517024f25ab335c6e47b5df");
+  // decide to render
+  let content = null;
+  // loader
+  if (isLoading && !isError) {
+    content = (
+      <div className="container">
+        <h2 className="text-center pt-10 alert alert-info mt-50">Loading...</h2>
+      </div>
+    );
+  }
 
-const CourseDetailsLeftSide = ({ courseData }) => {
-  // distructure courseData
-  const { category, date, img_bg, review, teacher_img, tutor_name, title } =
-    courseData;
-  // all courses
-  const courses = useSelector((state) => state.courses.allCourses);
+  if (!isLoading && !isError) {
+    content = <CourseDetailsArea courseData={course} />;
+  }
+
   return (
     <>
-      <div className="col-xxl-8 col-xl-8 col-lg-8">
-        <div className="course__wrapper">
-          <h1>{title}</h1>
-
-          <div className="course__tab-content mb-9">
-            <div className="tab-content" id="courseTabContent">
-              <div
-                className="tab-pane fade show active"
-                id="description"
-                role="tabpanel"
-                aria-labelledby="description-tab"
-              >
-                <div className="course__description">
-                  <h3>Highlights</h3>
-                  <table className="table table-striped style_table">
-                    <tbody>
-                      <tr>
-                        <td>
-                          <p>
-                            <strong>Full name</strong>
-                          </p>
-                        </td>
-                        <td>
-                          <p>
-                            International Management Institute IMI New Delhi
-                          </p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>
-                            <strong>Founded year</strong>
-                          </p>
-                        </td>
-                        <td>
-                          <p>1981</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>
-                            <strong>Located City</strong>
-                          </p>
-                        </td>
-                        <td>
-                          <p>New Delhi</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>
-                            <strong>Entrance Exam</strong>
-                          </p>
-                        </td>
-                        <td>
-                          <p>CAT</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>
-                            <strong>Approved by</strong>
-                          </p>
-                        </td>
-                        <td>
-                          <p>AICTE, NBA</p>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-
-              <div id="curriculum">
-                <div>
-                  <h3>
-                    <strong>
-                      International Management Institute Courses 2023
-                    </strong>
-                  </h3>
-                  <p>
-                    International Management Institute Delhi Courses include
-                    management study programme. Here are the details about the
-                    duration and fee structure of the particular courses.
-                  </p>
-                  <table className="table table-striped style_table">
-                    <tbody>
-                      <tr>
-                        <td>
-                          <p>
-                            <strong>Management Courses</strong>
-                          </p>
-                        </td>
-                        <td>
-                          <p>
-                            <strong>Years</strong>
-                          </p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDHRM</p>
-                        </td>
-                        <td>
-                          <p>2 years</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDM BFS</p>
-                        </td>
-                        <td>
-                          <p>2 years</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDM</p>
-                        </td>
-                        <td>
-                          <p>2 years</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDM Executive</p>
-                        </td>
-                        <td>
-                          <p>1 year</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>Fellow program in management</p>
-                        </td>
-                        <td>
-                          <p>4 years</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>Executive- FPM</p>
-                        </td>
-                        <td>
-                          <p>4 years</p>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <h3>
-                    <strong>IMI Delhi MBA Fees 2023</strong>
-                  </h3>
-                  <p>
-                    As mentioned above, the International Management Institute
-                    New Delhi is a reputed private business institute situated
-                    in National Capital. Here is the complete International
-                    Management Institute IMI Delhi MBA Fees 2023 that includes
-                    hostel fee and mess fee.
-                  </p>
-                  <table className="table table-striped style_table">
-                    <tbody>
-                      <tr>
-                        <td>
-                          <p>
-                            <strong>Courses</strong>
-                          </p>
-                        </td>
-                        <td>
-                          <p>
-                            <strong>Total Fees</strong>
-                          </p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDHRM</p>
-                        </td>
-                        <td>
-                          <p>18,34,900 INR</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDM BFS</p>
-                        </td>
-                        <td>
-                          <p>18,34,900 INR</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDM</p>
-                        </td>
-                        <td>
-                          <p>18,34,900 INR</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>PGDM Executive</p>
-                        </td>
-                        <td>
-                          <p>12,17,500 INR</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>Fellow Programme in Management</p>
-                        </td>
-                        <td>
-                          <p>Yet to be changed</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>
-                          <p>Executive- FPM</p>
-                        </td>
-                        <td>
-                          <p>502,000 INR (Excluding hostel)</p>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-              <div id="review">
-                <div>
-                  <h3>
-                    <strong>IMI Eligibility Criteria 2023</strong>
-                  </h3>
-                  <p>
-                    IMI Eligibility Criteria 2023 is compulsory to fulfill by
-                    the students seeking admission in the Institute. If any
-                    candidate does not manage to fulfill below-mentioned
-                    criteria then he/she will be considered as disqualified.
-                  </p>
-                  <ul>
-                    <li>
-                      50% marks in bachelors degree from a valid university or
-                      college.
-                    </li>
-                    <li>
-                      Some relaxation will be offered to eligible students
-                    </li>
-                    <li>
-                      Graduation final year students are allowed to apply for
-                      Admission but they have to submit their final documents
-                      before 30th Sept 2023.
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div id="member">
-                <h3>
-                  <strong>IMI Admission Process 2023</strong>
-                </h3>
-                <p>
-                  IMI Admission 2023 will be invited once the candidate fulfils
-                  the IMI eligibility criteria. Follow the information about the
-                  IMI Admission and submit important documents within given
-                  deadlines. Click Here for Getting complete information related
-                  to Admission 2023.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SEO pageTitle="Course Details" />
+      <Header />
+      <BreadCrumb title="Our Courses" subtitle="Courses" />
+      <DetailsTabItems />
+      {content}
+      <Footer />
     </>
   );
 };
 
-export default CourseDetailsLeftSide;
+export default CourseDetails;
